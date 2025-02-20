ffaeeb867319b5667e8d29b78a21dbe5a773ec1f
Argh-Real-Coding <maddiestanczak659@gmail.com>
2025-02-20 12:27:17 -0500

    Placeholder Website

---
+++ panes.js
@@ -0,0 +1,45 @@
+const panes = document.querySelectorAll('.pane')
+
+let z = 1
+
+panes.forEach((pane) => {
+  const title = pane.querySelector('.title')
+  const corner = pane.querySelector('.corner')
+
+  pane.addEventListener('mousedown', () => {
+    z = z + 1
+    pane.style.zIndex = z
+  })
+
+  title.addEventListener('mousedown', (event) => {
+    pane.classList.add('is-dragging')
+
+    let l = pane.offsetLeft
+    let t = pane.offsetTop
+
+    let startX = event.pageX
+    let startY = event.pageY
+
+    const drag = (event) => {
+      event.preventDefault()
+
+      pane.style.left = l + (event.pageX - startX) + 'px'
+      pane.style.top = t + (event.pageY - startY) + 'px'
+    }
+
+    const mouseup = () => {
+      pane.classList.remove('is-dragging')
+
+      document.removeEventListener('mousemove', drag)
+      document.removeEventListener('mouseup', mouseup)
+    }
+
+    document.addEventListener('mousemove', drag)
+    document.addEventListener('mouseup', mouseup)
+  })
+
+})
+
+function hide(doc) {
+  document.getElementById(doc).style.display='none';
+}
